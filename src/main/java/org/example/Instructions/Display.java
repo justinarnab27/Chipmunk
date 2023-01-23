0f1bca14a146f422ee1359f1934c08351e90be7a
Arnab Bala <justinarnab27@gmail.com>
2023-01-23 17:06:10 +0530

    Fixed display out of bound error

--- src/main/java/org/example/Instructions/Display.java
+++ src/main/java/org/example/Instructions/Display.java
@@ -1,6 +1,7 @@
 package org.example.Instructions;
 
 import lombok.NonNull;
+import org.example.Constants;
 import org.example.ProgramState;
 
 import static org.example.Constants.REGISTER_I;
@@ -18,8 +19,8 @@ public class Display {
 
         // Gets the x and y coordinate of the top left
         // corner of the sprite from registers VX and VY
-        int VX = programState.getRegister(X);
-        int VY = programState.getRegister(Y);
+        int VX = programState.getRegister(X) % Constants.WIDTH_IN_PIXELS;
+        int VY = programState.getRegister(Y) % Constants.HEIGHT_IN_PIXELS;
 
         // Gets the byte from address in register I
         int address = programState.getRegister(REGISTER_I);
@@ -38,6 +39,7 @@ public class Display {
 //            System.out.println("Com " + Integer.toBinaryString(b));
             for (int j = 0; j < 8; ++j) {
                 if (lsb_array[8 - j - 1] == 1) {     // Reversing the array order to get the msb first
+                    if (VY + i >= Constants.HEIGHT_IN_PIXELS || VX + j >= Constants.WIDTH_IN_PIXELS) continue;
                     int prev_pixel = programState.getDisplayMatrixPixel(VY + i, VX + j);
                     // Flips the pixel value if the bit is 1
                     if (prev_pixel == 0) {
