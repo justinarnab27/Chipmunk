5b481262e304ebd88fba1043b547e6163808c1b9
Arnab Bala <justinarnab27@gmail.com>
2023-01-24 18:24:49 +0530

    refactored

--- src/main/java/org/example/SourceReader.java
+++ src/main/java/org/example/SourceReader.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.nio.file.Path;
 import lombok.NonNull;
 
 import java.io.IOException;
@@ -13,18 +14,17 @@ public class SourceReader {
          * Takes a binary file as input (inputFile) and returns
          * an array of bytes
          */
-        //
-        inputFile = "/Users/arnab/Downloads/Pong [Paul Vervalin, 1990].ch8";
-//        inputFile = "/Users/arnab/Downloads/Tetris [Fran Dachille, 1991].ch8";
-//        inputFile = "/Users/arnab/Downloads/test_opcode.ch8";
-//        inputFile = "/Users/arnab/Downloads/IBM Logo.ch8";
-//        inputFile = "/Users/arnab/Downloads/tetris.rom";
-//        inputFile = "/Users/arnab/Downloads/Tic-Tac-Toe [David Winter].ch8";
-//        inputFile = "/Users/arnab/Downloads/Chip8 Picture.ch8";
+        String romName = "Pong [Paul Vervalin, 1990].ch8";
+//        String romName = "Tetris [Fran Dachille, 1991].ch8";
+//        String romName = "test_opcode.ch8";
+//        String romName = "IBM Logo.ch8";
+//        String romName = "Tic-Tac-Toe [David Winter].ch8";
+//        String romName = "Chip8 Picture.ch8";
 
-
+        String romDir = "./rom/";
+        Path path = Paths.get(romDir, romName);
         try {
-            byte[] array = Files.readAllBytes(Paths.get(inputFile));
+            byte[] array = Files.readAllBytes(path);
             return array;
         } catch (IOException e) {
             throw new RuntimeException(e);
